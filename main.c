5754349dcb4437b6621d3b4ad15d1f96232944cd
Gustavo Rondina <g.rondina@theo.chemie.tu-darmstadt.de>
2018-11-26 16:43:09 +0100

    Remove rigid-body rotation

--- main.c
+++ main.c
@@ -6,6 +6,9 @@
 #include <errno.h>
 #include <math.h>
 #include <float.h>
+#include <lapacke.h>
+#include <lapacke_utils.h>
+#include <cblas.h>
 
 /* 2D array indexing for row major order */
 #define ARRAY2D(A, n1, n2, N2) ((A)[(n2) + ((N2)*(n1))])
@@ -123,12 +126,12 @@ double read_mass(char *fn)
 
 }
 
-inline double distance(double x1, double y1, double z1, double x2, double y2, double z2)
+double distance(double x1, double y1, double z1, double x2, double y2, double z2)
 {
     return sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2) + (z1 - z2)*(z1 - z2));
 }
 
-inline double distance_sq(double x1, double y1, double z1, double x2, double y2, double z2)
+double distance_sq(double x1, double y1, double z1, double x2, double y2, double z2)
 {
     return ((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2) + (z1 - z2)*(z1 - z2));
 }
@@ -208,6 +211,193 @@ void chain_print_matrix(chain_t *c)
     }
 }
 
+void print_matrix(double *A, int M, int N)
+{
+    int i, j;
+
+    for (i = 0; i < M; i++) {
+        for (j = 0; j < N; j++) {
+            printf("% -6.4f%s", ARRAY2D(A, i, j, N), (j == N-1 ? "\n" : "  "));
+        }
+    }
+}
+
+void transpose(double *A, double *B, int M, int N)
+{
+    int i, j;
+
+    for (i = 0; i < M; i++) {
+        for (j = 0; j < N; j++) {
+            ARRAY2D(B, j, i, M) = ARRAY2D(A, i, j, N);
+        }
+    }
+}
+
+void multiply(double *A, double *B, double *C, int M, int N, int K)
+{
+    int i, j, k;
+    double s;
+
+    for (i = 0; i < M; i++) {
+        for (j = 0; j < N; j++) {
+            s = 0;
+            for (k = 0; k < K; k++) {
+                s += ARRAY2D(A, i, k, K)*ARRAY2D(B, k, j, N);
+            }
+            ARRAY2D(C, i, j, N) = s;
+        }
+    }
+}
+
+double det3x3rmo(double *A)
+{
+    return (A[0]*A[4]*A[8] + A[1]*A[5]*A[6] + A[2]*A[3]*A[7] - A[2]*A[4]*A[6] - A[1]*A[3]*A[8] - A[0]*A[5]*A[7]);
+}
+
+void kabsch(chain_t *c, chain_t *ref)
+{
+    int i, j;
+    int N;
+    double *P;
+    double *Pt;
+    double *Q;
+    double *A;
+
+    double *SIGMA;
+    double *U;
+    double *Vt;
+    double superb[10];
+
+    double *Ut;
+    double *V;
+    double d;
+    double M[9] = { 1, 0, 0, 0, 1, 0, 0, 0, 1 };
+
+    double *R;
+
+    assert(c->n == ref->n);
+    N = c->n;
+
+    P  = malloc(N * 3 * sizeof(double));
+    Pt = malloc(N * 3 * sizeof(double));
+    Q  = malloc(N * 3 * sizeof(double));
+    A  = malloc(3 * 3 * sizeof(double));
+
+    assert(P  != NULL);
+    assert(Pt != NULL);
+    assert(Q  != NULL);
+    assert(A  != NULL);
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < 3; j++) {
+            ARRAY2D(P, i, j, 3) = ARRAY2D(c->r, i, j, 3);
+            ARRAY2D(Q, i, j, 3) = ARRAY2D(ref->r, i, j, 3);
+        }
+    }
+
+    LAPACKE_dge_trans(LAPACK_ROW_MAJOR, N, 3, P, 3, Pt, N);
+    //transpose(P, Pt, N, 3);
+
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 3, 3, N, 1.0, Pt, N, Q, 3, 0, A, 3);
+    //multiply(Pt, Q, A, 3, 3, N);
+
+    SIGMA = malloc(3 * 3 * sizeof(double));
+    U = malloc(3 * 3 * sizeof(double));
+    Vt = malloc(3 * 3 * sizeof(double));
+
+    //printf("--A--\n");
+    //print_matrix(A, 3, 3);
+    //printf("---\n");
+
+    LAPACKE_dgesvd(LAPACK_ROW_MAJOR, 'A', 'A', 3, 3, A, 3, SIGMA, U, 3, Vt, 3, superb);
+
+    //printf("**SIGMA**\n");
+    //print_matrix(SIGMA, 3, 3);
+
+    //printf("==U==\n");
+    //print_matrix(U, 3, 3);
+
+    //printf("==Vt==\n");
+    //print_matrix(Vt, 3, 3);
+
+    Ut = malloc(3 * 3 * sizeof(double));
+    V  = malloc(3 * 3 * sizeof(double));
+
+    assert(Ut != NULL);
+    assert(V  != NULL);
+
+    LAPACKE_dge_trans(LAPACK_ROW_MAJOR, 3, 3, U, 3, Ut, 3);
+    LAPACKE_dge_trans(LAPACK_ROW_MAJOR, 3, 3, Vt, 3, V, 3);
+
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 3, 3, 3, 1.0, V, 3, Ut, 3, 0, A, 3);
+
+
+    d = det3x3rmo(A);
+
+    M[8] = d;
+    //printf("---M---\n");
+    //print_matrix(M, 3, 3);
+
+    R = malloc(3 * 3 * sizeof(double));
+    assert(R != NULL);
+
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 3, 3, 3, 1.0, V, 3, M, 3, 0, A, 3);
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 3, 3, 3, 1.0, A, 3, Ut, 3, 0, R, 3);
+
+    //printf("~~~~~~~~~~~~~~~~~~~~~~~~~~ R ~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    //print_matrix(R, 3, 3);
+    //printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 3, N, 3, 1.0, R, 3, Pt, N, 0, Q, N);
+
+    LAPACKE_dge_trans(LAPACK_ROW_MAJOR, 3, N, Q, N, P, 3);
+
+    //printf("________P FINAL_______\n");
+    //print_matrix(P, N, 3);
+    //printf("^^^^^^^^^^^^^^^^^^^^^^\n");
+
+    /* replace the old coordinates */
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < 3; j++) {
+            ARRAY2D(c->r, i, j, 3) = ARRAY2D(P, i, j, 3);
+        }
+    }
+}
+
+void compute_mwcm(double *sigma, double *sigmap, chain_t *c, double m)
+{
+    int i, j;
+    double *M;
+    double n;
+
+    n = c->n;
+
+    M = malloc(n * n * sizeof(double));
+    assert(M != NULL);
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (i == j) {
+                ARRAY2D(M, i, j, n) = sqrt(m);
+            } else {
+                ARRAY2D(M, i, j, n) = 0;
+            }
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (i == j) {
+                ARRAY2D(M, i, j, n) = sqrt(m);
+            } else {
+                ARRAY2D(M, i, j, n) = 0;
+            }
+        }
+    }
+
+    free(M);
+}
+
 int main(int argc, char *argv[])
 {
     int i, j, k;
@@ -222,6 +412,7 @@ int main(int argc, char *argv[])
     double rg_min;
     double xj, yj, zj;
     double xk, yk, zk;
+    double *sigma, *sigmap;
 
     for (i = 0; i < NCHAINS; i++) {
         chains[i].n = clen;
@@ -315,6 +506,21 @@ int main(int argc, char *argv[])
         chain_convert(&ref);
         //printf("--- ref ---\n");
         //chain_print_matrix(&ref);
+        
+        /* Remove rigid-body rotation */
+        for (i = 0; i < NCHAINS; i++) {
+            kabsch(&chains[i], &ref);
+        }
+
+        for (i = 0; i < NCHAINS; i++) {
+            sigma = malloc(chains[i].n * chains[i].n * sizeof(double));
+            sigmap = malloc(chains[i].n * chains[i].n * sizeof(double));
+            assert(sigma != NULL);
+            assert(sigmap != NULL);
+            compute_mwcm(sigma, sigmap, &chains[i], mass);
+            free(sigmap);
+            free(sigma);
+        }
         
         break;
     }
